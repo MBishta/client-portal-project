e44de725df83089ab8a034cebacf7b865cb10e23
Mohamed Bishta <mohamedbishta@MacBook-Pro.local>
2026-04-27 14:52:18 +0300

    Add client model and admin

--- clients/models.py
+++ clients/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Client(models.Model):
+    client_name = models.CharField(max_length=150)
+    company_name = models.CharField(max_length=150, blank=True, null=True)
+    phone = models.CharField(max_length=30)
+    email = models.EmailField(blank=True, null=True)
+    address = models.TextField(blank=True, null=True)
+    is_active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.client_name
