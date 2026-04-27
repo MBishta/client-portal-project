fcd56b6d38df5d39c42dfeb436f3e02c7a58418a
Mohamed Bishta <mohamedbishta@MacBook-Pro.local>
2026-04-27 13:40:52 +0300

    Add login page

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from . import views
+from .views import CustomLoginView
 
 app_name = 'accounts'
 
 urlpatterns = [
+    path('login/', CustomLoginView.as_view(), name='login'),
 ]
