1cbea84e1d92d2d88038594905ce2cb95190e9b8
Mohamed Bishta <mohamedbishta@MacBook-Pro.local>
2026-04-27 13:54:35 +0300

    Add dashboard page

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import CustomLoginView
+from .views import CustomLoginView, dashboard_view
 
 app_name = 'accounts'
 
 urlpatterns = [
     path('login/', CustomLoginView.as_view(), name='login'),
+    path('dashboard/', dashboard_view, name='dashboard'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,12 @@
 from django.contrib.auth.views import LoginView
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render
 
 
 class CustomLoginView(LoginView):
     template_name = 'accounts/login.html'
+
+
+@login_required
+def dashboard_view(request):
+    return render(request, 'accounts/dashboard.html')
