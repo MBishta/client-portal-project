23ea922282ab1f03605548849d24d171a7336ebd
Mohamed Bishta <mohamedbishta@MacBook-Pro.local>
2026-05-02 13:06:15 +0300

    Add engineer detail page

--- engineers/urls.py
+++ engineers/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import engineer_list_view
+from .views import engineer_list_view, engineer_detail_view
 
 app_name = 'engineers'
 
 urlpatterns = [
     path('', engineer_list_view, name='engineer_list'),
+    path('<int:pk>/', engineer_detail_view, name='engineer_detail'),
 ]
--- engineers/views.py
+++ engineers/views.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
 from .models import Engineer
 
@@ -13,4 +13,19 @@ def engineer_list_view(request):
 
     return render(request, 'engineers/engineer_list.html', {
         'engineers': engineers
+    })
+
+
+@login_required
+def engineer_detail_view(request, pk):
+    if request.user.role != 'ADMIN':
+        engineer = None
+        projects = []
+    else:
+        engineer = get_object_or_404(Engineer, pk=pk)
+        projects = engineer.projects.all()
+
+    return render(request, 'engineers/engineer_detail.html', {
+        'engineer': engineer,
+        'projects': projects,
     })
