03e5250b4a634270f433208ee171e3284661c97f
Mohamed Bishta <mohamedbishta@MacBook-Pro.local>
2026-05-05 14:28:41 +0300

    Add pagination to projects list

--- projects/views.py
+++ projects/views.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.decorators import login_required
 from django.db.models import Q
+from django.core.paginator import Paginator
 from django.shortcuts import get_object_or_404, redirect, render
 
 from .forms import (
@@ -43,8 +44,13 @@ def project_list_view(request):
             Q(assigned_engineers__engineer_name__icontains=search_query)
         ).distinct()
 
+    paginator = Paginator(projects, 25)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
     return render(request, 'projects/project_list.html', {
-        'projects': projects,
+        'projects': page_obj,
+        'page_obj': page_obj,
         'status_filter': status_filter,
         'search_query': search_query,
     })
